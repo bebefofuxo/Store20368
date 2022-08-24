e3504ccdcea0e6337cbf35e0b00c65763704c18a
bebefofuxo <38541246+bebefofuxo@users.noreply.github.com>
2022-08-24 11:07:22 -0300

    Sent by DeployFC at 24/08/2022 11:07:22

---
+++ htm/js/config.js
@@ -0,0 +1,22 @@
+/*Configurações gerais da loja*/
+
+var oStoreConfig$={
+"Show_idCountryFC":[false,"Exibe o campo idCountryFC 'País' no checkout"],
+"Show_idBirthdayFC":[true,"Exibe o campo idBirthdayFC 'Data nascimento' no checkout"],
+"Show_idPassRemFC":[false,"Exibe o campo idPassRemFC 'Lembrete de senha' no checkout"],
+"Show_idCellPhoneFC":[false,"Exibe o campo idCellPhoneFC 'Celular' no checkout"],
+"Show_idGenderFC":[false,"Exibe o campo idGenderFC 'Sexo' no checkout"],
+"Show_idAccInfoFC":[false,"Exibe título idAccInfoFC 'Dados para identificação' no checkout"],
+"Show_idTxtCEP1FC":[false,"Exibe idTxtCEP1FC 'Clique se não souber CEP' no checkout"],
+"Show_idLocalFC":[true,"Exibe o campo idLocalFC 'Local' em outro endereço de entrega do checkout"],
+"Show_idParaFC":[false,"Exibe o campo idParaFC 'Para' em outro endereço de entrega do checkout"],
+"Show_idPEmailFC":[false,"Exibe o campo idPEmailFC 'E-mail' em outro endereço de entrega do checkout"],
+"Show_idPPaisFC":[false,"Exibe o campo idPPaisFC 'País' em outro endereço de entrega do checkout"],
+"Show_idTxtCEP2FC":[false,"Exibe idTxtCEP2FC 'Clique se não souber CEP' em outro endereço de entrega do checkout"],
+"Show_idTxtCPFFC":[false,"Para clientes B2C: Exibe texto idTxtCPFFC 'CPF opcional'"],
+"Show_idOccupationFC":[false,"Para clientes B2C: Exibe o campo idOccupationFC 'Profissão' no checkout"],
+"Show_idTxtCPFFC_PJ":[false,"Para clientes B2B: Exibe texto idTxtCPFFC 'CNPJ opcional'"],
+"Show_idOccupationFC_PJ":[false,"Para clientes B2B: Exibe o campo idOccupationFC 'Ramo de Atuação' no checkout"],
+"Show_idFAXFC_PJ":[false,"Para clientes B2B: Exibe o campo idFAXFC 'Fax' no checkout"],
+"Show_TinyCheckout":[true,"Exibe checkout resumido após preenchimento"]
+};
